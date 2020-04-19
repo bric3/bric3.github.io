6ed5ac5e604bdcdcb7386b74d78f428eb3e52ad1
Brice Dutheil <brice.dutheil@gmail.com>
2020-04-19 02:46:24 +0200

    WIP Allow to build asciidoctor files locally

--- serve.sh
+++ serve.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "Outdated for now, please use `hugo serve` instead"
-exit 1
+#exec docker run --name blog --tty --rm --volume "$PWD":/usr/src/app:delegated --volume site:/usr/src/app/_site --publish "4000:4000" starefossen/github-pages jekyll serve -d /_site --watch --future --incremental --force_polling -H 0.0.0.0 -P 4000
 
-exec docker run --name blog --tty --rm --volume "$PWD":/usr/src/app:delegated --volume site:/usr/src/app/_site --publish "4000:4000" starefossen/github-pages jekyll serve -d /_site --watch --future --incremental --force_polling -H 0.0.0.0 -P 4000
+env PATH=$PWD/bin:$PATH hugo serve --buildDrafts --baseUrl=blog.local --bind=0.0.0.0
