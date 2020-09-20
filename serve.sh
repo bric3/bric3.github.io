cfcf4521e6d4a8f7b80e02acf40850646bb9ca23
Brice Dutheil <brice.dutheil@gmail.com>
2020-09-20 15:28:10 +0200

    Upgrade to Hugo 0.75.1 and asciidoctor-diagram 2.0.3
    Starting Hugo 0.74.0 you can pass attributes to asciidoctor
    via the config.toml file.
    
    With asciidoctor-diagram 2.0.3, I can use the diagram-svg-type attribute
    which allows to get rid of the
    
     - imagesdir / imagesoutdir attributes
     - format=svg,opts=inline local attributes

--- serve.sh
+++ serve.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # slow but use the same build as used on Github Actions
-#exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts
+#exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts --destination ./public
 
-exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture
+#exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./resources/_gen/diagram
+rm -rf .asciidoctor/ public/
+exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./public
