f400c87e036f9bf018737e9341e179a19d8c0f54
Brice Dutheil <brice.dutheil@gmail.com>
2021-01-27 16:47:59 +0100

    Update asciidiagram with smetana instead of jdot

--- serve.sh
+++ serve.sh
@@ -3,6 +3,6 @@
 # slow but use the same build as used on Github Actions
 #exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts --destination ./public
 
-#exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./resources/_gen/diagram
+exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./resources/_gen/diagram
 rm -rf .asciidoctor/ public/
-exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./public
+#exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./public
