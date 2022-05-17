5f6808cc6d828f5fba84a0388a6b47520ddfc367
Brice Dutheil <brice.dutheil@gmail.com>
2022-05-17 14:16:43 +0200

    doc: Readme Setup

--- serve.sh
+++ serve.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# slow but use the same build as used on Github Actions
-#exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts --destination ./public
-
 rm -rf .asciidoctor/ public/
-exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./resources/_gen/diagram
+# slow but use the same build as used on Github Actions
+exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts --destination ./public
+
+#exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./resources/_gen/diagram
 #exec hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture --destination ./public
